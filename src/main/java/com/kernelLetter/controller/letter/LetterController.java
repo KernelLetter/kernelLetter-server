192cfeca27bddc7bc769f3d127bc110bfff9d804
김동균 <eastVirusDa@gmail.com>
2025-11-23 15:46:49 +0900

    코드레빗 먼저 구현할게요
    # Conflicts:
    #	src/main/java/com/kernelLetter/controller/letter/LetterController.java

--- src/main/java/com/kernelLetter/controller/letter/LetterController.java
+++ src/main/java/com/kernelLetter/controller/letter/LetterController.java
@@ -15,4 +15,15 @@ public class LetterController {
     public void sendLetter(@RequestBody LetterSendDto dto) {
         letterService.sendLetter(dto);
     }
+
+    // 편지 수정하기
+    @PatchMapping("/{receiverId}")
+    public void update
+
+    // 편지 삭제하기
+
+
+    // 편지 조회하기
+
+
 }
