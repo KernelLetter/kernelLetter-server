81b0513d86e90845cfb4d9701aa489d0108e6e8b
CHO GUNHEE <106148197+dkqpeo@users.noreply.github.com>
2025-12-21 12:44:57 +0900

    fix : ec2 서버시간 한국시간으로 변경 (#30)

--- src/main/java/com/kernelLetter/KernelLetterApplication.java
+++ src/main/java/com/kernelLetter/KernelLetterApplication.java
@@ -1,12 +1,21 @@
 package com.kernelLetter;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class KernelLetterApplication {
+
+	@PostConstruct
+	public void init() {
+		// 애플리케이션 전체의 기본 타임존을 한국 시간으로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KernelLetterApplication.class, args);
